9243165cb3f7cdd5956c7a81055e83fecffaa9e1
podervivo <emilianogarello@MacBook-Pro-de-Emiliano.local>
2025-01-29 10:35:05 -0300

    manejo de formularios utilizando urls dinamicas con flask

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
+@app.route("/user/<string:username>")
+def greet_user(username):
+    return f"Hola, {username}! Bienvenido a Flask."
+
 
 @app.route('/')
 def index():
     return render_template("index.html", nombre="ivana" )
+3
 
 
+@app.route("/form")
+def form():
+    return render_template("form.html")
 
-@app.route('/about')
-def about():
-    return "hola"
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    return f"¡Hola, {name}! Tu formulario fue enviado correctamente."
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
